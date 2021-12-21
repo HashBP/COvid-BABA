be6ae85b3f7875f735f939409233403be2116cb9
Harsh <Hvp6848@gmail.com>
2021-12-22 02:47:30 +0530

    Created Layout for the app

--- app/src/main/java/com/example/gdscandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/gdscandroidproject/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.gdscandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var anim :TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        anim =findViewById(R.id.textview3)
+        anim.setSelected(true)
     }
 }
