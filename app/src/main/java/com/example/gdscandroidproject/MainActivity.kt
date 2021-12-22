b61a26dcb906c6ba2f005da209de426d9c276d39
Harsh <Hvp6848@gmail.com>
2021-12-22 12:49:10 +0530

    Finalised the UI

--- app/src/main/java/com/example/gdscandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/gdscandroidproject/MainActivity.kt
@@ -2,14 +2,27 @@ package com.example.gdscandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.content.Intent
+import android.net.Uri
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    lateinit var anim :TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        anim =findViewById(R.id.textview3)
-        anim.setSelected(true)
+    }
+
+    fun toCoWin(view: View) {
+        val url = "https://selfregistration.cowin.gov.in/"
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+    }
+
+    fun toWho(view: View) {
+        val url = "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/technical-guidance"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 }
